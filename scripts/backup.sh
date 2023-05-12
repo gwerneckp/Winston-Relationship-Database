11538e7910cda41a46b582b2e41f054906f5198e
gwerneckpaiva <gwerneckpaiva@gmail.com>
2023-05-12 16:53:23 +0100

    Changed backup and load scripts, less workers for guinicorn

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -3,4 +3,4 @@
 data_dir=$(pwd)/data
 backups_dir=$(pwd)/backups
 
-docker run --interactive --tty --rm --volume=$data_dir:/data --volume=$backups_dir:/backups neo4j/neo4j-admin:5.7.0 neo4j-admin database dump neo4j --to-path=/backups --verbose
+docker run --interactive --tty --rm --volume="$data_dir:/data" --volume="$backups_dir:/backups" neo4j:4.4.20 neo4j-admin dump --database=neo4j --to=/backups/neo4j.dump
--- scripts/load.sh
+++ scripts/load.sh
@@ -3,4 +3,4 @@
 data_dir=$(pwd)/data
 backups_dir=$(pwd)/backups
 
-docker run --interactive --tty --rm --volume=$data_dir:/data --volume=$backups_dir:/backups neo4j/neo4j-admin:5.7.0 neo4j-admin database load neo4j --from-path=/backups --verbose
+docker run --interactive --tty --rm --volume=./data:/data --volume=./backups:/backups neo4j:4.4.20 neo4j-admin load --from=/backups/neo4j.dump --database=neo4j
